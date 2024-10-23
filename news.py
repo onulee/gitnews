95ef076e3b6471835f01fc35186ef2ea09ed3a4a
onulee <65047894+onulee@users.noreply.github.com>
2024-10-23 22:35:12 +0900

    Update news.py

--- news.py
+++ news.py
@@ -1,20 +1,28 @@
 import requests
 from bs4 import BeautifulSoup
-import pandas as pd
+import time
+import csv
+
 url = "https://finance.naver.com/sise/lastsearch2.naver"
-headers = {"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36"}
+headers = {"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36",
+    'Accept-Language':'ko-KR,ko;q=0.9,en-US;q=0.8,en;q=0.7'}
 res = requests.get(url,headers=headers)
-res.raise_for_status()
 soup = BeautifulSoup(res.text,"lxml")
-data = soup.select_one("div#contentarea")
-type1 = data.select_one("tr.type1")
-th_datas = type1.select("th")
 
-th_list = []
-for th_data in th_datas:
-    th_list.append(th_data.text)
-    print(th_data.text)
-dic = {"증권제목":th_list} 
-df = pd.DataFrame(dic)
-df.to_csv("data.csv",encoding="utf-8",index=False) 
+# 기준점
+data = soup.select_one('#contentarea > div.box_type_l > table')
+stocks = data.select("tr")
 
+# 1.상단타이틀. csv파일로 저장
+f = open('stock.csv','w',encoding='utf-8-sig',newline="")
+writer = csv.writer(f)
+st_list = [ st.text  for st in stocks[0].select("th") ]
+writer.writerow(st_list)
+# 30개 주식정보를 csv파일로 저장
+for stock in stocks:
+  sts = stock.select("td")
+  if len(sts) <= 1: continue
+  sto_list = [ st.text.strip().replace("\t","").replace("\n","")  for st in sts ]
+  writer.writerow(sto_list) #writerow 리스트타입을 저장
+
+f.close()
